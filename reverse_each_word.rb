ab4a71d4f2db8f9f3a800b3d84146018c91f93e9
Jack Quinoa <jquinoa208@gmail.com>
2020-04-01 02:23:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,7 @@ require "pry"
 string = "Hello there, and how are you?"
 def reverse_each_word(string)
   word_array = string.split(" ")
-  word_array.collect do |word| word.reverse!
+  word_array.collect do |word| word.reverse
   end
   word_array.join(" ")
 end
